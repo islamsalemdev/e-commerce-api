753c5cf844e18612927648ba34b9a8332493a171
Islam Rabie <eslam.salem@hive-one.net>
2023-12-26 18:46:32 +0200

    #12

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ mongoose
 
 // Connect to the Port
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("Servers is working successfully");
 });
