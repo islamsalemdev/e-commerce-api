08cb29e01b8bc7bebabb340ee381d13ba796a89c
Islam Rabie <eslam.salem@hive-one.net>
2023-12-26 19:28:15 +0200

    uploads folder

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const langRouter = require("./routers/lang");
 
 //middleware
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //localization
 i18n.configure({
