bc08cc0d92a8053948d70a40ca97991fd65397ac
islamrabi3 <146248853+islamsalemdev@users.noreply.github.com>
2024-09-20 21:14:16 +0300

    #bearer 4

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -5,7 +5,7 @@ const admin = async (req, res, next) => {
   const token = req.header("Authorization").split(" ")[1];
   if (!token) return res.status(401).json({ error: "Access denied" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.PASSWORD_KEY, (err, user) => {
     if (err) return res.status(403).json({ error: "Invalid token" });
     if (user.isAdmin === false) {
       return res.status(403).json({
